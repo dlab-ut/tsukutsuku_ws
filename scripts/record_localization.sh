ca5f9e13cc17181d5375479b6353424e5c11558c
tsukutsuku <tsukutsuku@roboken.iit.tsukuba.ac.jp>
2022-10-24 18:38:17 +0900

    update rosbag scripts

--- scripts/record_localization.sh
+++ scripts/record_localization.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 TODAY=`date "+%Y%m%d"`;
-if [ ! -d /home/tsukutsuku/data/bag/ros2/localization/$TODAY ]; then
-    mkdir -p /home/tsukutsuku/data/bag/ros2/localization/$TODAY
+MEDIA=false
+if [ -e /media/tsukutsuku/extreme_ssd ]; then
+    MEDIA=true
 fi
-TIME=`date "+%H-%M-%S"`;
 
+if "${MEDIA}"; then
+    SAVE_DIR="/media/tsukutsuku/extreme_ssd/ros2bag/localization"
+else
+    SAVE_DIR="/home/tsukutsuku/data/bag/ros2/localization"
+fi
+
+if [ ! -d $SAVE_DIR/$TODAY ]; then
+    mkdir -p $SAVE_DIR/$TODAY
+fi
+
+TIME=`date "+%H-%M-%S"`;
 source /home/tsukutsuku/tsukutsuku_ws/install/setup.bash
 
-ros2 bag record --compression-mode file --compression-format zstd -b 3000000000 -o /home/tsukutsuku/data/bag/ros2/localization/$TODAY/ros2bag_$TIME /ypspur/twist /ypspur/twist_with_covariance /ypspur/odom /sensing/velodyne_lower/rectified/pointcloud_ex /sensing/microstrain_imu/gx5/imu/data /sensing/microstrain_imu/gx5/nav/filtered_imu/data  /sensing/microstrain_imu/gx5/device/status /sensing/microstrain_imu/gx5/nav/odom /ypspur/pose_with_covariance /ypspur/pose /tf_static /tf
+ros2 bag record -o $SAVE_DIR/$TODAY/ros2bag_$TIME /ypspur/twist /ypspur/twist_with_covariance /ypspur/odom /sensing/velodyne_lower/rectified/pointcloud_ex /sensing/velodyne_lower/rectified/pointcloud /sensing/microstrain_imu/gx5/imu/data /sensing/microstrain_imu/gx5/nav/filtered_imu/data  /sensing/microstrain_imu/gx5/device/status /sensing/microstrain_imu/gx5/nav/odom /ypspur/pose_with_covariance /ypspur/pose /tf_static /tf /sensing/center_camera/image_raw
--- scripts/record_navigation.sh
+++ scripts/record_navigation.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 TODAY=`date "+%Y%m%d"`;
-if [ ! -d /home/tsukutsuku/data/bag/ros2/navigation/$TODAY ]; then
-    mkdir -p /home/tsukutsuku/data/bag/ros2/navigation/$TODAY
+MEDIA=false
+if [ -e /media/tsukutsuku/extreme_ssd ]; then
+    MEDIA=true
 fi
+
+if "${MEDIA}"; then
+    SAVE_DIR="/media/tsukutsuku/extreme_ssd/ros2bag/navigation"
+else
+    SAVE_DIR="/home/tsukutsuku/data/bag/ros2/navigation"
+fi
+
+if [ ! -d $SAVE_DIR/$TODAY ]; then
+    mkdir -p $SAVE_DIR/$TODAY
+fi
+
 TIME=`date "+%H-%M-%S"`;
 
 source /home/tsukutsuku/tsukutsuku_ws/install/setup.bash
 
-ros2 bag record --compression-mode file --compression-format zstd -b 3000000000 -o /home/tsukutsuku/data/bag/ros2/navigation/$TODAY/ros2bag_$TIME /ypspur/twist /ypspur/twist_with_covariance /ypspur/odom /sensing/velodyne_lower/rectified/pointcloud_ex /sensing/microstrain_imu/gx5/imu/data /sensing/microstrain_imu/gx5/nav/filtered_imu/data  /sensing/microstrain_imu/gx5/device/status /sensing/microstrain_imu/gx5/nav/odom /ypspur/pose_with_covariance /ypspur/pose /tf_static /tf /behavior_tree_log /cmd_vel /diagnostics /global_costmap/costmap /global_costmap/footprint /goal_pose /global_costmap/published_footprint /local_costmap/costmap /local_costmap/footprint  /local_costmap/published_footprint /local_costmap/voxel_grid /local_plan /localization/pose_estimator/pose_with_covariance /localization/pose_twist_fusion_filter/pose_with_covariance /localization/pose_twist_fusion_filter/pose_with_covariance_without_yawbias /localization/twist_estimator/twist /localization/util/downsampled/pointcloud /map /plan /received_global_plan /transformed_global_plan /speed_limit /waypoints 
+ros2 bag record -o $SAVE_DIR/$TODAY/ros2bag_$TIME /ypspur/twist /ypspur/twist_with_covariance /ypspur/odom /sensing/velodyne_lower/rectified/pointcloud_ex /sensing/microstrain_imu/gx5/imu/data /sensing/microstrain_imu/gx5/nav/filtered_imu/data  /sensing/microstrain_imu/gx5/device/status /sensing/microstrain_imu/gx5/nav/odom /ypspur/pose_with_covariance /ypspur/pose /tf_static /tf /behavior_tree_log /cmd_vel /diagnostics /global_costmap/costmap /global_costmap/footprint /goal_pose /global_costmap/published_footprint /local_costmap/costmap /local_costmap/footprint  /local_costmap/published_footprint /local_costmap/voxel_grid /local_plan /localization/pose_estimator/pose_with_covariance /localization/pose_twist_fusion_filter/pose_with_covariance /localization/pose_twist_fusion_filter/pose_with_covariance_without_yawbias /localization/twist_estimator/twist /localization/util/downsampled/pointcloud /map /plan /received_global_plan /transformed_global_plan /speed_limit /waypoints /sensing/center_camera/image_raw
